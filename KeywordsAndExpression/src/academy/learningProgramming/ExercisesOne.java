454b67834827f0557eb2c0586cbf9fd2675763fe
Tongyang Ni <ntyaiqingr@gmail.com>
2020-06-23 22:29:01 -0400

    complete first exercise

--- KeywordsAndExpression/src/academy/learningProgramming/ExercisesOne.java
+++ KeywordsAndExpression/src/academy/learningProgramming/ExercisesOne.java
@@ -4,6 +4,7 @@ public class ExercisesOne {
 
     public static void main(String[] args) {
         System.out.println(toMilesPerHour(75.114));
+        printConversion(100.4);
     }
     public static long toMilesPerHour(double kilometersPerHour) {
         if(kilometersPerHour < 0) {
@@ -14,6 +15,11 @@ public class ExercisesOne {
      }
 
      public static void printConversion(double kilometersPerHour) {
+        if (kilometersPerHour >= 0) {
+            System.out.println(kilometersPerHour + " km/h = " + Math.round(kilometersPerHour/1.609) + " mi/h");
+        } else {
+            System.out.println("Invalid Value");
+        }
 
      }
 }
