fa2bdd8d17bf54c979be47c3d90bf84492892903
Tongyang Ni <ntyaiqingr@gmail.com>
2020-06-30 22:58:33 -0400

    method overloading

---
+++ KeywordsAndExpression/src/academy/learningProgramming/MethodOverloading.java
@@ -0,0 +1,26 @@
+package academy.learningProgramming;
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New Score is " + newScore);
+        calculateScore(75);
+        calculateScore();
+
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score);
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        System.out.println("Unnamed player scored " + score);
+        return score * 1000;
+    }
+
+    public static int calculateScore() {
+        System.out.println("No player name and score");
+        return 0;
+    }
+}
