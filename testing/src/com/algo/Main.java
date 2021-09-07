0901bfabe809a4bad2b828f6e16c63c70ce0c9c6
Tongyang Ni <ntyaiqingr@gmail.com>
2021-09-07 00:59:39 -0400

    testing array and my ssh key

---
+++ testing/src/com/algo/Main.java
@@ -0,0 +1,19 @@
+package com.algo;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[] ids = new int[]{1,2,3,4};
+
+	    for (int i=0; i<ids.length; i++){
+            System.out.println(ids[i]);
+        }
+
+        String[] names = new String[5];
+	    names[0] = "name0";
+        for (int i=0; i<names.length; i++){
+            System.out.println(names[i]);
+        }
+
+    }
+}
