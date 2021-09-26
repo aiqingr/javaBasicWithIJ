423f08b7f7b864f92b7b8e38140092128f90f373
Tongyang Ni <ntyaiqingr@gmail.com>
2021-09-26 01:06:58 -0400

    merge sort

---
+++ hashmaphashset/src/com/algo/MergeSort.java
@@ -0,0 +1,157 @@
+package com.algo;
+
+public class MergeSort {
+//    递归的版本
+
+    public static void mergeSort1(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        process(arr, 0, arr.length - 1);
+
+    }
+    // L...R N T(N) = 2*T(N/2) + O(N ^ 1) a = 2 b = 2 d = 1
+    // O(NlogN  )
+    public static void process(int[] arr, int L, int R) {
+        if (L == R) { // base case
+            return;
+        }
+        int mid = L + ((R - L) >> 1);
+        process(arr, L, mid);
+        process(arr, mid + 1, R);
+        merge(arr, L, mid, R);
+    }
+
+    public static void merge(int[] arr, int L, int M, int R) {
+        int[] help = new int[R - L + 1];
+        int i = 0;
+        int p1 = L;
+        int p2 = M + 1;
+        while (p1 <= M && p2 <= R) {
+            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+        }
+//            if (arr[p1] <= arr[p2]) {
+//                help[i] = arr[p1];
+//                i++;
+//                p1++;
+//            } else {
+//                help[i] = arr[p2];
+//                i++;
+//                p2++;
+//            }
+        while (p1 <= M) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= R) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[L + i] = help[i];
+        }
+
+    }
+
+//    非递归的版本
+    public static void  mergeSort2(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        int N = arr.length;
+        int mergeSize = 1; //当前有序的左组长度
+        while (mergeSize < N) {
+            // 当前左组的第一个位置
+            int L = 0;
+            while (L < N) {
+                // L...M 左组（mergeSize）
+                int M = L + mergeSize - 1;
+                if (M >= N) {
+                    break;
+                }
+                //L...M M+1...R
+                int R = Math.min(M + mergeSize, N - 1);
+                merge(arr, L, M, R);
+                // 下一个左组的第一个位置
+                L = R + 1;
+            }
+            // 防止溢出
+            if (mergeSize > N / 2) {
+                break;
+            }
+            mergeSize <<= 1;
+        }
+
+    }
+
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 1000;
+        int maxSize = 100;
+        int maxValue = 100;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+
+            mergeSort1(arr1);
+            mergeSort2(arr2);
+
+            if (!isEqual(arr1, arr2)) {
+                System.out.println("出错了！");
+                printArray(arr1);
+                printArray(arr2);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
+}
